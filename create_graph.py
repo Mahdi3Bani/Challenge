77cc0f373f4ecd21312330ed374bcc9ec8015e64
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-13 23:54:22 +0100

    still in progress...

---
+++ create_graph.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sys
+
+data = pd.read_json('C:\Users\banim\Desktop\CHALLENGE\Challenge\parsed_data.7z')
+
+
+
+
+def create_graph(progress, data):
+  #function to createa graph
